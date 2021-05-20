440d38a840579fa8528f1d387bc8b5dc1d421778
Badar Teknog <82695671+BadarTeknog@users.noreply.github.com>
2021-05-19 23:38:52 -1200

    Update PushButon.ino

--- Arduino-PushButon/PushButon/PushButon.ino
+++ Arduino-PushButon/PushButon/PushButon.ino
@@ -1,1 +1,23 @@
-https://github.com/BadarTeknog/Arduino-Program/blob/main/Arduino-PushButon/PushButon.ino
+byte data;
+void setup(){
+ Serial.begin(9600);
+ pinMode(11,INPUT_PULLUP);
+ pinMode(13,OUTPUT);  
+    delay(1000); 
+ 
+}
+void loop(){
+  data=digitalRead(11);
+    
+  Serial.println(data);
+      
+  if(data==0){
+        digitalWrite(13,HIGH);
+    }else{
+        digitalWrite(13,LOW);
+    } 
+    
+}
+// Jangan lupa subscribe nya 
+// Badar Teknog
+// video pembahasanhttps://youtu.be/Heeir4xSBwQ
